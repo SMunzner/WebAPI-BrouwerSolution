fb03cb356e6dd5259b780b43381c5bfbbfb09dfa
SimonM <munzner.simon@gmail.com>
2024-03-07 13:31:07 +0100

    DELETE, POST, PUT

--- BrouwerService/Controllers/BrouwerController.cs
+++ BrouwerService/Controllers/BrouwerController.cs
@@ -2,6 +2,7 @@
 using BrouwerService.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BrouwerService.Controllers
 {
@@ -9,6 +10,9 @@ namespace BrouwerService.Controllers
     [ApiController]
     public class BrouwerController(IBrouwerRepository repository) : ControllerBase
     {
+
+        /*------------------------GET--------------------------------------*/
+
         [HttpGet]
         public IActionResult FindAll() => base.Ok(repository.FindAll());
 
@@ -27,5 +31,50 @@ namespace BrouwerService.Controllers
         [HttpGet("naam")]
         public IActionResult FindByBeginNaam(string begin) =>
             base.Ok(repository.FindByBeginNaam(begin));
+
+
+        /*-----------------------------DELETE--------------------------------------*/
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var brouwer = repository.FindById(id);
+            if(brouwer == null)
+                return base.NotFound();
+
+            repository.Delete(brouwer);
+            return base.Ok();
+        }
+
+        /*------------------------------POST------------------------------------------*/
+
+        [HttpPost]
+        public IActionResult Post(Brouwer brouwer)
+        {
+            repository.Insert(brouwer);
+            return base.CreatedAtAction(nameof(FindById), new { id = brouwer.Id }, null);
+        }
+
+
+        /*----------------------------PUT---------------------------------------------*/
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Brouwer brouwer)
+        {
+            try
+            {
+                brouwer.Id = id;
+                repository.Update(brouwer);
+                return base.Ok();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return base.NotFound();
+            }
+            catch
+            {
+                return base.Problem();
+            }
+        }
     }
 }
